ae109cb9784ee965ae21c36cfeb0429138b635e5
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-20 09:01:17 +0530

    Welcome to spring address book app

---
+++ src/main/java/com/bridgelabz/app/SpringAddressBookApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAddressBookApplication.class, args);
+		System.out.println("Welcome to the SpringAddressBookApplication ");
+	}
+
+}
